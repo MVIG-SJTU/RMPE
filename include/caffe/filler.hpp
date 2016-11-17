a731a8c8a4153c135f1c73c83a854a380de1867d
Fang-Haoshu <510641426@qq.com>
2016-11-17 21:21:40 +0800

    merge file filler @daerduoCarey

--- include/caffe/filler.hpp
+++ include/caffe/filler.hpp
@@ -308,6 +308,29 @@ class NearestUpsamplingFiller : public Filler<Dtype> {
 };
 
 /**
+ * @brief Use file to initialize the weights or bias
+ */
+template <typename Dtype>
+class FileFiller : public Filler<Dtype> {
+ public:
+  explicit FileFiller(const FillerParameter& param)
+      : Filler<Dtype>(param) {}
+  virtual void Fill(Blob<Dtype>* blob) {
+	  CHECK(this->filler_param_.has_file());
+	  std::ifstream file(this->filler_param_.file().c_str());
+	  Dtype* data = blob->mutable_cpu_data();
+	  int count = blob->count();
+	  Dtype temp;
+	  for(int i=0; i<count; ++i) {
+		  file >> temp;
+		  data[i] = temp;
+		  std::cout << "Setting " << i << "th position to " << temp << std::endl;
+	  }
+	  CHECK_EQ(this->filler_param_.sparse(), -1)
+	           << "Sparsity not supported by this Filler.";
+  }
+};
+/**
  * @brief Get a specific filler from the specification given in FillerParameter.
  *
  * Ideally this would be replaced by a factory pattern, but we will leave it
@@ -332,6 +355,8 @@ Filler<Dtype>* GetFiller(const FillerParameter& param) {
     return new BilinearFiller<Dtype>(param);
   } else if (type == "nearest") {
     return new NearestUpsamplingFiller<Dtype>(param);
+  }else if (type == "file") {
+    return new FileFiller<Dtype>(param);
   }else {
     CHECK(false) << "Unknown filler name: " << param.type();
   }
